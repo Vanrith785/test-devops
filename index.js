bd724b798c76360376746bd651532b301e605b5a
testinggithub <pichvanrith785@gmail.com>
2025-09-13 16:50:08 +0700

    change index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+// index.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
+});
